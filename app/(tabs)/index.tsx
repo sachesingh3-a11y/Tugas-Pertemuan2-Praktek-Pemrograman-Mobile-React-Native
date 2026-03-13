5c518a27d977c7bedab295bf70472108e7b7a290
Sache Deep Singh <sachesingh3@gmail.com>
2026-03-13 15:54:50 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,71 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      {/* SECTION FOTO */} 
+      <Image 
+        source={{ uri: "https://raw.githubusercontent.com/evtaindra/my-app-test/refs/heads/master/assets/images/gambar22.png" }} 
+        
+         //  bisa juga menggunakan local image
+        //  image: source={require('../assets/images/gambar22.png')}
+
+        style={styles.profilePic} 
+      />
+
+      {/* SECTION IDENTITAS */}
+      <Text style={styles.name}>Mobile Dev Sensei</Text>
+      <Text style={styles.nim}>NIDN: 14118302</Text>
+
+      {/* SECTION BIO */}
+      <View style={styles.bioCard}>
+        <Text style={styles.bioText}>
+          🚀 Currently mastering React Native SDK 50. 
+          Goal: Build goated apps!
+        </Text>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  profilePic: {
+    width: 140,
+    height: 140,
+    borderRadius: 70, // Bikin jadi bulat
+    borderWidth: 3,
+    borderColor: '#ffff99',
+    marginBottom: 15,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: '900',
+    color: '#fff',
+    marginBottom: 5,
+  },
+  nim: {
+    fontSize: 14,
+    color: '#ffff99',
+    marginBottom: 20,
+    letterSpacing: 2,
+  },
+  bioCard: {
+    backgroundColor: '#111',
+    padding: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#333',
+    width: '100%',
+  },
+  bioText: {
+    color: '#ccc',
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
+});
